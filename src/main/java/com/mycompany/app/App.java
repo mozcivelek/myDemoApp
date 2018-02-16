bc676390219c2530683aebd5a70b190a756b7ac7
mozcivelek <mozcivelek@gmail.com>
2018-02-16 19:23:51 +0300

    I have started my awesome projectgit statusgit status It will be great

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null) return false;
+  
+        for (int elt : array) {
+          if (elt == e) return true;
+        }
+        return false;
+      }
+}

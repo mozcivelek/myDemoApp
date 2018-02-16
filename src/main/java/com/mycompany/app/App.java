1acbd7abdb6c1c544023543693b0bd0cc513b6c2
mozcivelek <mozcivelek@gmail.com>
2018-02-16 21:38:03 +0300

    rebuilding2.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,74 +1,27 @@
-package com.mycompany.app;
-
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.post;
+package main.java.com.mycompany.app;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import spark.ModelAndView;
-import spark.template.mustache.MustacheTemplateEngine;
+import java.util.Collections;
 
 public class App 
 {
-
-     public static void main(String[] args) {
-        port(getHerokuAssignedPort());
-
-        get("/", (req, res) -> "Hello, World");
-
-        post("/compute", (req, res) -> {
-          //System.out.println(req.queryParams("input1"));
-          //System.out.println(req.queryParams("input2"));
-	  String input1 = req.queryParams("input1");
-          java.util.Scanner sc1 = new java.util.Scanner(input1);
-          sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-          while (sc1.hasNext())
-          {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-            inputList.add(value);
-          }
-          System.out.println(inputList);
-
-
-          String input2 = req.queryParams("input2").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
-
-          boolean result = App.search(inputList, input2AsInt);
-
-         Map map = new HashMap();
-          map.put("result", result);
-          return new ModelAndView(map, "compute.mustache");
-        }, new MustacheTemplateEngine());
-
-
-        get("/compute",
-            (rq, rs) -> {
-              Map map = new HashMap();
-              map.put("result", "not computed yet!");
-              return new ModelAndView(map, "compute.mustache");
-            },
-            new MustacheTemplateEngine());
-    }
-
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
-    public static boolean search(ArrayList<Integer> array, int e) {
-        System.out.println("inside search");
-        if (array == null) return false;
-  
-        for (int elt : array) {
-          if (elt == e) return true;
-        }
-        return false;
-      }
+	
+public static boolean calculate(ArrayList<Integer> arr1,ArrayList<Integer> arr2,Integer i1,Integer i2){
+	ArrayList<Integer> newarr=new ArrayList<Integer>();
+	for(Integer i:arr1) {
+		newarr.add(i);
+	}
+	for(Integer j:arr2) {
+		newarr.add(j);
+	}
+	int counter=0;
+	Collections.sort(newarr);
+	for(Integer t:newarr) {
+		if(t.equals(i1))
+			counter++;
+	}
+	if(counter==i2)
+		return true;
+	return false;
 }
-
+}

8be3053507335054f72e27ac931cfa29307d7513
mozcivelek <mozcivelek@gmail.com>
2018-02-16 23:12:33 +0300

    Changed files2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,31 +1,29 @@
 package com.mycompany.app;
-import java.util.*;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
-import static spark.Spark.*;
-public class App {
-	public static boolean append_search(ArrayList<Integer> array1, ArrayList<Integer> array2, int e) {
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		if(array1 == null && array2 == null)
-			return false;
-		if(array1 != null) {
-			for(int x : array1)
-				array.add(x);
-		}
-		if(array2 != null || !array2.isEmpty()) {
-			for(int y : array2)
-				array.add(y);
-		}
-				
-		if(array != null || !array.isEmpty()) {
-			for(int a : array) {
-			if(a==e)
-				return true;
-			}
-		}
-		return false;
-	}
-public static void main(String[] args) {
+
+public class App
+{
+    public static boolean search(ArrayList<Integer> array, int e) {
+      System.out.println("inside search");
+      if (array == null) return false;
+
+      for (int elt : array) {
+        if (elt == e) return true;
+      }
+      return false;
+    }
+
+    public static void main(String[] args) {
         port(getHerokuAssignedPort());
 
         get("/", (req, res) -> "Hello, World");
@@ -33,35 +31,23 @@ public static void main(String[] args) {
         post("/compute", (req, res) -> {
           //System.out.println(req.queryParams("input1"));
           //System.out.println(req.queryParams("input2"));
-	  //System.out.println(req.queryParams("input3"));
 
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList1 = new java.util.ArrayList<>();
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
           while (sc1.hasNext())
           {
             int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-            inputList1.add(value);
+            inputList.add(value);
           }
-          System.out.println(inputList1);
-
-	String input2 = req.queryParams("input2");
-          java.util.Scanner sc2 = new java.util.Scanner(input2);
-          sc2.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
-          while (sc2.hasNext())
-          {
-            int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
-            inputList2.add(value);
-          }
-          System.out.println(inputList2);
+          System.out.println(inputList);
 
 
-          String input3 = req.queryParams("input3").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input3);
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          int input2AsInt = Integer.parseInt(input2);
 
-          boolean result = App.append_search(inputList1, inputList2, input2AsInt);
+          boolean result = App.search(inputList, input2AsInt);
 
          Map map = new HashMap();
           map.put("result", result);
@@ -75,7 +61,7 @@ public static void main(String[] args) {
               map.put("result", "not computed yet!");
               return new ModelAndView(map, "compute.mustache");
             },
-	new MustacheTemplateEngine());
+            new MustacheTemplateEngine());
     }
 
     static int getHerokuAssignedPort() {
@@ -85,6 +71,5 @@ public static void main(String[] args) {
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
-
-
 }
+
